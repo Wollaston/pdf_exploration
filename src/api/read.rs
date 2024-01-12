475c3f6ca8a70ecbb751335f7a04c05661051f13
Wollaston <mattflynn1997@gmail.com>
2024-01-12 17:55:56 -0500

    Use aho_corasick instead of bstr for byte searches

--- src/api/read.rs
+++ src/api/read.rs
@@ -1,16 +1,11 @@
-use std::{
-    fmt::Write,
-    fs::{self, File},
-    io::{self, Read},
-    str,
-};
+use std::{fs, io, str};
 
+use aho_corasick::AhoCorasick;
 use bstr::ByteSlice;
 use nom::{
-    bytes::complete::{tag, take, take_while1},
-    character::is_alphabetic,
+    bytes::complete::{tag, take},
     sequence::Tuple,
-    AsBytes, IResult,
+    IResult,
 };
 
 use super::structure::Header;
@@ -21,43 +16,57 @@ pub fn read_bytes(path: &str) -> Result<Vec<u8>, io::Error> {
     Ok(f)
 }
 
-pub fn read_header(data: &Vec<u8>) -> IResult<&str, Header> {
-    // The defined pattern to match against use bstr
-    let pattern = "%PDF-";
+pub fn read_header(data: &[u8], start_offset: usize) -> IResult<&[u8], Header> {
     // The bytes offset defined in the PDF standrd from the initial header byte
     // to End
-    let offset = 15; // Needs to be 16, but for testing it's 15 due to non-UTF8 error
-    let mut matches = vec![];
-    for tag in data.find_iter(pattern) {
-        matches.push(tag);
-    }
-    let index = matches[0];
-    let slice = &data[index..=offset];
-    let slice = str::from_utf8(slice).unwrap();
+    let end_offset = 15; // Needs to be 16, but for testing it's 15 due to non-UTF8 error
+    let slice = &data[start_offset..=end_offset];
+    // let slice = str::from_utf8(slice).unwrap();
     println!("{slice:?}");
 
     let clear_pdf_tag = tag("%PDF-");
     let take3 = take(3usize);
-    let (input, (_, version)) = (clear_pdf_tag, take3).parse(slice)?;
+    let take2 = take(2usize);
+
+    let (input, (_, version, _)) = (clear_pdf_tag, take3, take2).parse(slice)?;
     let header = Header {
-        version: version.to_string(),
+        version: version.to_str().unwrap().to_string(),
         comment: None,
     };
+    println!("Version: {:?}", header.version);
 
     Ok((input, header))
+}
+
+pub fn read_file(path: &str) -> Result<(), io::Error> {
+    let data = read_bytes(path)?;
+
+    let patterns = &["%PDF-", "endstream"];
+    let haystack = data.as_slice();
+
+    let ac = AhoCorasick::new(patterns).unwrap();
+    let mut matches = vec![];
+    for mat in ac.find_iter(haystack) {
+        matches.push((mat.pattern(), mat.start(), mat.end()));
+    }
+    let header_offset: Vec<_> = matches.iter().filter(|&x| x.0.as_usize() == 0).collect();
+    println!("Header Offset: {:?}", header_offset);
+    println!("Matches: {:?}", matches);
+
+    read_header(&data, header_offset[0].0.as_usize());
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use std::io;
 
-    use super::{read_bytes, read_header};
+    use super::{read_bytes, read_file, read_header};
 
     #[test]
-    fn test_header() -> Result<(), io::Error> {
-        let data = read_bytes("HelloWorld.pdf")?;
-        let (input, major) = read_header(&data).unwrap();
-        println!("Input: {input:?}\nMajor: {major:?}");
+    fn test_file() -> Result<(), io::Error> {
+        read_file("HelloWorld.pdf");
         Ok(())
     }
 }
