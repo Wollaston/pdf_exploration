53a7c9a3ad0f550555cb65d424db33e96322b56a
Wollaston <mattflynn1997@gmail.com>
2024-01-11 20:54:05 -0500

    Update doc comments formatting

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@
 //! using Rust.
 //!
 //! The general idea:
-//!     - A PDF is a binary format, so read it in as bytes
-//!     - Use a crate like bstr to read through the bytes
+//! - A PDF is a binary format, so read it in as bytes
+//! - Use a crate like bstr to read through the bytes
 //!         and determine the byte offset of keyword delimiters
 //!         of PDF objects (this is done using a crate like bstr
 //!         as not all bytes are valid UTF-8)
-//!     - Once the byte offset and type of object match is determined,
+//! - Once the byte offset and type of object match is determined,
 //!         use custom nom functions to parse the data into defined
 //!         structs representing the key data in the PDF objects
 //!
