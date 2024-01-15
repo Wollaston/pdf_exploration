980eb319320e4d9005112792d7d4092115ae8547
Wollaston <mattflynn1997@gmail.com>
2024-01-15 13:56:20 -0500

    Update documentation for reading of xref table

--- src/api/read/cross_ref_table.rs
+++ src/api/read/cross_ref_table.rs
@@ -10,7 +10,29 @@ use nom::{
 
 use crate::api::structure::{self, CrossRefTable, Subsection, SubsectionEntry};
 
-pub fn read_xref(data: &[u8], start_offset: usize) -> IResult<&[u8], structure::CrossRefTable> {
+/// The cross-reference table begins with the keyword "xref" and contains one or more
+/// cross-reference sections.
+///
+/// Each cross-reference section begins with two integers representing a contiguous
+/// range of objects. The integers are seperated by a space and together terminated by
+/// and EOL character. The first integer represents the object number of the first
+/// object in the subsection, and the second the numer of entries in the subsection.
+/// For example, "10 2" denotes a subsection starting with object number 10 and spanning
+/// two objects; accordingly, this subsection consists of objets 10 and 11.
+///
+/// Each section consists of the noted number of subsections, in the format of:
+/// nnnnnnnnnn ggggg n eol
+///
+/// Where:
+/// - nn..nn is the 10-digit byte offset of the object
+/// - ggggg is the 5-digit generation number
+/// - n is a keyword identifying if the entry is in use or not
+///     - n denotes in-use; f denotes a free entry
+/// - a 2-digit EOL sequence
+pub fn read_cross_ref_table(
+    data: &[u8],
+    start_offset: usize,
+) -> IResult<&[u8], structure::CrossRefTable> {
     let take_obj_number = map_res(digit1, |d: &[u8]| d.to_str().unwrap().parse::<usize>());
     let take_space = take(1usize);
     let take_num_objects = map_res(digit1, |d: &[u8]| d.to_str().unwrap().parse::<usize>());
@@ -25,7 +47,7 @@ pub fn read_xref(data: &[u8], start_offset: usize) -> IResult<&[u8], structure::
     )
         .parse(slice)?;
 
-    let (input, subsection) = read_xref_subsection(input, obj_number, num_objects).unwrap();
+    let (input, subsection) = read_cross_ref_section(input, obj_number, num_objects).unwrap();
     let subsections: Vec<Subsection> = vec![subsection];
 
     let cross_ref_table = CrossRefTable { subsections };
@@ -33,23 +55,29 @@ pub fn read_xref(data: &[u8], start_offset: usize) -> IResult<&[u8], structure::
     Ok((input, cross_ref_table))
 }
 
-pub fn read_xref_subsection(
+/// Each cross-reference table section begins with two integers, separated by a
+/// space and terminated by an eol character, noting the object number of the first
+/// object and the number of entries in the section.
+pub fn read_cross_ref_section(
     data: &[u8],
     start_offset: usize,
-    num_subsections: usize,
+    num_entries: usize,
 ) -> IResult<&[u8], Subsection> {
     let slice = &data[start_offset..];
 
-    let (input, subsections) = read_subsections(slice, num_subsections).unwrap();
+    let (input, subsections) = read_subsections(slice, num_entries).unwrap();
     let subsection = Subsection {
         object_number: start_offset,
-        num_entries: num_subsections,
+        num_entries,
         entries: subsections,
     };
 
     Ok((input, subsection))
 }
 
+/// Each cross-reference table section contains one more more subsections.
+/// The number of subsections is denoted in the section header as the second
+/// integer.
 pub fn read_subsections(
     input: &[u8],
     num_subsections: usize,
@@ -57,6 +85,15 @@ pub fn read_subsections(
     count(read_subsection, num_subsections)(input)
 }
 
+/// Each cross-reference table sub-section consists of three main parts in the format of:
+/// nnnnnnnnnn ggggg n eol
+///
+/// Where:
+/// - nn..nn is the 10-digit byte offset of the object
+/// - ggggg is the 5-digit generation number
+/// - n is a keyword identifying if the entry is in use or not
+///     - n denotes in-use; f denotes a free entry
+/// - a 2-digit EOL sequence
 pub fn read_subsection(input: &[u8]) -> IResult<&[u8], SubsectionEntry> {
     map(
         tuple((
